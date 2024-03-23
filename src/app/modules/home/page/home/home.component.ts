9eb5c2308a82e8c3facfb7efdc1afc9980a7c09a
makaruos-mekhail <https://makaruosmekhail@gmail.com>
2024-03-23 23:37:03 +0200

    update3

--- src/app/modules/home/page/home/home.component.ts
+++ src/app/modules/home/page/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeDarkService } from 'src/app/shared/Theme_dark/theme-dark.service';
 
 @Component({
@@ -6,7 +6,43 @@ import { ThemeDarkService } from 'src/app/shared/Theme_dark/theme-dark.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  sentence: string = "Welcome to my porotfolio !. I'am a passionate frontend developer dedicated . to creating exceptional user experience. With a strong background in Angular , HTML5 ,. CSS3 , JavaScript and Dot Net Developer";
+  delay: number = 70;
+  output: string = '';
+
+
+  ngOnInit(): void {
+    this.printSentence();
+  }
+
+  printSentence() {
+    const words = this.sentence.split(' ');
+    let i = 0;
+    let j = 0;
+    const intervalId = setInterval(() => {
+      if (words[i][j] === ' ') {
+        this.output += '&nbsp;';
+      }
+      else if (words[i][j] === '.') {
+        this.output += '<br>'; 
+      } else {
+        this.output += `<span>${words[i][j]}</span>`;
+      }
+      j++;
+      if (j >= words[i].length) {
+        i++;
+        j = 0;
+        this.output += ' ';
+      }
+      if (i >= words.length) {
+        clearInterval(intervalId);
+      }
+    }, this.delay);
+  }
+
+
   constructor(private themeService: ThemeDarkService) {
   }
   
