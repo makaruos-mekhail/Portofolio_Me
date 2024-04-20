79744210aa77ffd83103f77f4e4073afd7dd5f19
makaruos-mekhail <https://makaruosmekhail@gmail.com>
2024-04-21 00:06:32 +0200

    update 040000

--- src/app/shared/Theme_dark/theme-dark.service.ts
+++ src/app/shared/Theme_dark/theme-dark.service.ts
@@ -4,18 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ThemeDarkService {
-  isDarkMode: boolean = false;
+  isDarkMode: boolean = true;
 
   constructor() {
     const storedDarkMode = localStorage.getItem('DarkMode');
-    this.isDarkMode = storedDarkMode === 'false';
+    // this.isDarkMode = storedDarkMode === 'true';
+
+    if (localStorage.getItem('DarkMode')) {
+      this.isDarkMode = storedDarkMode === 'true';
+    } else {
+      this.isDarkMode = storedDarkMode === 'false';
+    };
   }
   
 
+
    toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
-    localStorage.setItem('DarkMode', this.isDarkMode ? 'false' : 'true');
+    localStorage.setItem('DarkMode', this.isDarkMode ? 'true' : 'false');
   }
+
+
+  
+  
+
 
   getCurrentThemeClass(baseClass: string): string {
     return this.isDarkMode ? `${baseClass}_light` : baseClass;
