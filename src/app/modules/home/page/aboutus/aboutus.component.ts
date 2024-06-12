c8a405339e8b233ec5c4357b7dcc5b6b7c131bdc
makaruos-mekhail <https://makaruosmekhail@gmail.com>
2024-06-12 23:31:40 +0300

    update 0500

--- src/app/modules/home/page/aboutus/aboutus.component.ts
+++ src/app/modules/home/page/aboutus/aboutus.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.scss']
 })
-export class AboutusComponent implements OnInit {
+export class AboutusComponent  implements OnInit {
   image?: string;
   productName?: string;
-
-  constructor(private route: ActivatedRoute,private router: Router,private clipboard: Clipboard) {}
-
 
   projectDetails = {
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsoZHAOACc4Tpme0cvaxjdRYckM024_fxWBw&s',
     productName: 'makaruos Product'
   };
 
-  // constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private clipboard: Clipboard,
+    private meta: Meta,
+    private titleService: Title
+  ) {}
 
   shareLink() {
     const url = new URL(window.location.href);
@@ -28,12 +32,20 @@ export class AboutusComponent implements OnInit {
     this.clipboard.copy(url.toString());
   }
 
-
-  
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.image = params['image'];
-      this.productName = params['productName'];
+      this.image = params['image'] || this.projectDetails.image;
+      this.productName = params['productName'] || this.projectDetails.productName;
+  
+      const validImage = this.image || 'default-image.jpg';
+      const validProductName = this.productName || 'Default Product Name';
+  
+      // تحديث Meta Tags
+      this.titleService.setTitle(validProductName);
+      this.meta.updateTag({ property: 'og:title', content: validProductName });
+      this.meta.updateTag({ property: 'og:description', content: `Check out this product: ${validProductName}` });
+      this.meta.updateTag({ property: 'og:image', content: validImage });
+      this.meta.updateTag({ property: 'og:url', content: window.location.href });
     });
   }
 
